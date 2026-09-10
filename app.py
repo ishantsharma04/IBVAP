ff586e147d8b69a5900c6843276c9bf8a113a560
ishantsharma04 <ishant678sharma@gmail.com>
2026-09-10 15:26:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -201,4 +201,7 @@ def offline():
 @app.post("/clear")
 def clear(): events.clear(); return jsonify({"ok":True})
 
-if __name__=="__main__": app.run(host="127.0.0.1",port=5000,debug=False,threaded=True)
+import os
+
+if _name=="main_":
+    app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
